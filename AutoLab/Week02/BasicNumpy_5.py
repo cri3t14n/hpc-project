2f436cb6ddd1c8ebc43263473673440a91eabb54
Cristian Placinta <cristianplacinta04@gmail.com>
2026-04-21 10:41:10 +0200

    Autolab week02 fixed

--- AutoLab/Week02/BasicNumpy_5.py
+++ AutoLab/Week02/BasicNumpy_5.py
@@ -3,12 +3,12 @@ import numpy as np
 from time import perf_counter
 
 matrix = np.load(sys.argv[1])
-n = int(sys.argv[2])
+p = int(sys.argv[2])
 
 start = perf_counter()
 
 result = matrix.copy()
-for _ in range(n - 1):
+for _ in range(p):
     result = result @ matrix
 
 end = perf_counter()
