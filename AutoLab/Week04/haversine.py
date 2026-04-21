7d693d873b44effc5c44189bc55a0e0048949246
Cristian Placinta <cristianplacinta04@gmail.com>
2026-04-21 15:27:15 +0200

    Week04 finished

--- AutoLab/Week04/haversine.py
+++ AutoLab/Week04/haversine.py
@@ -1,6 +1,8 @@
 import sys
 import numpy as np
 
+
+# @profile
 def distance_matrix(p1, p2):
     p1, p2 = np.radians(p1), np.radians(p2)
 
@@ -15,20 +17,43 @@ def distance_matrix(p1, p2):
     D *= 6371  # Earth radius in km
     return D
 
-
+# @profile
 def distance_matrix_one_loop(p1, p2):
     p1, p2 = np.radians(p1), np.radians(p2)
+
+    cos_p2 = np.cos(p2[:, 0])
 
     D = np.empty((len(p1), len(p2)))
     for i in range(len(p1)):
         dsin2 = np.sin(0.5 * (p1[i] - p2)) ** 2
-        cosprod = np.cos(p1[i, 0]) * np.cos(p2[:, 0])
+        cosprod = np.cos(p1[i, 0]) * cos_p2
         a = dsin2[:, 0] + cosprod * dsin2[:, 1]
-        D[i, :] = 2 * np.arctan2(np.sqrt(a), np.sqrt(1 - a))
+        D[i, :] = 2 * np.arcsin(np.sqrt(a))
 
     D *= 6371  # Earth radius in km
     return D
 
+
+@profile
+def distance_matrix_no_loop(p1, p2):
+    p1, p2 = np.radians(p1), np.radians(p2)
+
+    lat1 = p1[:, 0][:, None]
+    lon1 = p1[:, 1][:, None]
+    lat2 = p2[:, 0][None, :]
+    lon2 = p2[:, 1][None, :]
+
+    dlat = lat1 - lat2
+    dlon = lon1 - lon2
+
+    dsin2_lat = np.sin(0.5 * dlat) ** 2
+    dsin2_lon = np.sin(0.5 * dlon) ** 2
+
+    a = dsin2_lat + np.cos(lat1) * np.cos(lat2) * dsin2_lon
+    D = 2 * np.arcsin(np.sqrt(a))
+
+    D *= 6371  # Earth radius in km
+    return D
 
 def load_points(fname):
     data = np.loadtxt(fname, delimiter=',', skiprows=1, usecols=(1, 2))
@@ -50,6 +75,6 @@ def distance_stats(D):
 
 fname = sys.argv[1]
 points = load_points(fname)
-D = distance_matrix_one_loop(points, points)
+D = distance_matrix_no_loop(points, points)
 stats = distance_stats(D)
 print(stats)
--- AutoLab/Week04/submit.sh
+++ AutoLab/Week04/submit.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-#BSUB -J hav_profile
+#BSUB -J hav_lineprof
 #BSUB -q hpc
 #BSUB -n 1
 #BSUB -W 00:20
 #BSUB -R "rusage[mem=4096]"
 #BSUB -R "select[model == XeonGold6126]"
-#BSUB -o /zhome/aa/6/205647/hpc-project/batch_outputs/hav_profile_%J.out
-#BSUB -e /zhome/aa/6/205647/hpc-project/batch_errors/hav_profile_%J.err
+#BSUB -o /zhome/aa/6/205647/hpc-project/batch_outputs/hav_lineprof_%J.out
+#BSUB -e /zhome/aa/6/205647/hpc-project/batch_errors/hav_lineprof_%J.err
 
 source /dtu/projects/02613_2025/conda/conda_init.sh
 conda activate 02613
 
-python -m cProfile -s cumtime haversine.py /dtu/projects/02613_2025/data/locations/locations_100.csv
+kernprof -l haversine.py /dtu/projects/02613_2025/data/locations/locations_100.csv
+python -m line_profiler -rmt haversine.py.lprof
